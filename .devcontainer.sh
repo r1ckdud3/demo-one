46386cd93770810eefc6a3bd62f242adb262e84c
r1ckdud3 <derek.d.maxey@lmco.com>
2025-11-13 09:40:47 -0600

    Update .devcontainer.sh

--- .devcontainer.sh
+++ .devcontainer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 apt-get -qq -y -y update
 apt-get -qq -y -y upgrade
-apt-get -qq -y -y install curl emacs file git gpg gzip sed tar vim offsec-pwk
+apt-get -qq -y -y install curl emacs file git gpg gzip sed tar vim build-essential
+# apt-get -qq -y -y offsec-pwk
