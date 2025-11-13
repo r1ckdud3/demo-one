44ce573cbac55476475d2efb7ddc7558354d4d61
r1ckdud3 <derek.d.maxey@lmco.com>
2025-11-13 12:37:32 -0600

    Update .devcontainer.sh

--- .devcontainer.sh
+++ .devcontainer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 apt-get -qq -y -y update
 apt-get -qq -y -y upgrade
-apt-get -qq -y -y install curl emacs file git gpg gzip sed tar vim build-essential
+apt-get -qq -y -y install curl dialog emacs file git gpg gzip sed tar vim build-essential
+apt-get -qq -y -y kali-linux-core ncat nmap
 # apt-get -qq -y -y offsec-pwk
